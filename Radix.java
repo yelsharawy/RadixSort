262d95a144ebd6f3761946d3c7bd4dcb42f13991
yelsharawy <yelsharawy20@stuy.edu>
2021-01-13 13:09:36 -0500

    Updated length and radixSortSimple for optimizing speed

--- Radix.java
+++ Radix.java
@@ -11,23 +11,7 @@ public class Radix {
     }
     
     public static int length(int n) {
-        /* This "Math" approach is ***WAY*** too slow. I timed it.
-         * While mine can do all possible integer values in 1.5 s,
-         * this one took over 70 seconds before I killed it.
-         *
-         * EDIT: I noticed this was probably due to branch prediction,
-         * so I tested it again with random values. Sure enough,
-         * the speeds were much more comparable, but my approach
-         * was still clearly faster.
-        return n == 0 ? 1 : (int)(Math.log10(Math.abs(n)) + 1);
-        */
-        n = Math.abs(n);
-        for (int i = 1; i < pow10.length; i++) {
-            if (n < pow10[i]) {
-                return i;
-            }
-        }
-        return pow10.length;
+        return Math.max(1, (int)Math.log10(Math.abs(n)) + 1);
     }
     
     public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
@@ -37,21 +21,24 @@ public class Radix {
     }
     
     public static void radixSortSimple(SortableLinkedList data) {
-        int maxLength = 0;
-        for (int i = 0; i < data.size(); i++) {
-            int value = data.remove(0);  // for O(n) traversal
-            maxLength = Math.max(maxLength, length(value));
-            data.add(value);
-        }
+        int maxLength = 1;
         
         SortableLinkedList[] buckets = new SortableLinkedList[10];
         for (int i = 0; i < buckets.length; i++) {
             buckets[i] = new SortableLinkedList();
         }
         
-        for (int i = 0; i < maxLength; i++) {
+        while (data.size() > 0) {
+            int value = data.remove(0);
+            maxLength = Math.max(maxLength, length(value));
+            int digit = nth(value, 0);
+            buckets[digit].add(value);
+        }
+        merge(data, buckets);
+        
+        for (int i = 1; i < maxLength; i++) {
             while (data.size() > 0) {
-                int value = data.remove(0);  // for O(n) traversal & clearing
+                int value = data.remove(0);
                 int digit = nth(value, i);
                 buckets[digit].add(value);
             }
@@ -66,7 +53,7 @@ public class Radix {
         SortableLinkedList positives = new SortableLinkedList();
         
         while (data.size() > 0) {
-            int value = data.remove(0);  // for O(n) traversal & clearing
+            int value = data.remove(0);
             if (value < 0) {
                 negatives.add(0, value);  // reversed
             } else {
